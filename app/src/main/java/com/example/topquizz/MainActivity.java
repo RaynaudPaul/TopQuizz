66361bd3b405d7a7334c62ef8367d89a50f2db99
Paul <raynaud.paul@educ-valadon-limoges.fr>
2022-09-29 14:23:10 +0200

    Premier push

--- app/src/main/java/com/example/topquizz/MainActivity.java
+++ app/src/main/java/com/example/topquizz/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.topquizz;
 
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText mEditText;
+    Button mButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.edit_text);
+        mButton = findViewById(R.id.button);
+
     }
 }
